eff8ad549909909684a6e18cca1f36564c3dccf1
Jinho Hyeon <neverlish@gmail.com>
2025-02-22 16:31:31 +0900

    NODE_ENV

--- lib/request.ts
+++ lib/request.ts
@@ -1,1 +1,1 @@
-export const BASE_URL = process.env.VERCEL_ENV ? "https://v0-next-js-react-layout.vercel.app/api" : "http://localhost:3000";
+export const BASE_URL = process.env.NODE_ENV ? "https://v0-next-js-react-layout.vercel.app/api" : "http://localhost:3000";
