524a6af8eea0983df47859f5fd5d7f860b6e5ee5
miachoidev <mia.choi.dev@gmail.com>
2025-03-02 15:14:19 +0900

    https

--- components/DifyChatButton.tsx
+++ components/DifyChatButton.tsx
@@ -46,7 +46,7 @@ const DifyChatModal = ({
 
     // console.log("encodedValue:", encodedValue);
 
-    const iframeUrl = `http://5310-58-143-233-101.ngrok-free.app/chatbot/${chatbotId}?product=${urlSafeEncodedValue}`;
+    const iframeUrl = `https://5310-58-143-233-101.ngrok-free.app/chatbot/${chatbotId}?product=${urlSafeEncodedValue}`;
     console.log("final URL:", iframeUrl);
 
     return (
